3345feb78b668433408eb2dde0bb4146e8ac6c92
Hilman Fikry <hilman12fikry@gmail.com>
2025-07-07 14:13:16 +0700

    refactor: shorten mathed theme color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 import ThemeColors from "./ThemeColors";
 
 export default function Header(props) {
+  const matchedColor = props.colors.find(
+    (color) => color.id === props.colorState,
+  ).oklch;
   return (
     <header className="flex items-center py-3">
       <svg
@@ -8,17 +11,23 @@ export default function Header(props) {
         viewBox="0 0 24 24"
         className="mr-2 size-8"
         style={{
-          fill: props.colors.find((color) => color.id === props.colorState)
-            .oklch,
+          fill: matchedColor,
         }}
       >
-        <path d="M13,9H18.5L13,3.5V9M6,2H14L20,8V20A2,2 0 0,1 18,22H6C4.89,22 4,21.1 4,20V4C4,2.89 4.89,2 6,2M14,20V19C14,17.67 11.33,17 10,17C8.67,17 6,17.67 6,19V20H14M10,12A2,2 0 0,0 8,14A2,2 0 0,0 10,16A2,2 0 0,0 12,14A2,2 0 0,0 10,12Z" />
+        <path
+          d={
+            "M13,9H18.5L13,3.5V9M6,2H14L20,8V20A2," +
+            "2 0 0,1 18,22H6C4.89,22 4,21.1 4,20V4C4,2.89 4.89," +
+            "2 6,2M14,20V19C14,17.67 11.33,17 10,17C8.67,17 6," +
+            "17.67 6,19V20H14M10,12A2,2 0 0,0 8,14A2,2 0 0,0 10," +
+            "16A2,2 0 0,0 12,14A2,2 0 0,0 10,12Z"
+          }
+        />
       </svg>
       <h1
         className="text-2xl font-bold"
         style={{
-          color: props.colors.find((color) => color.id === props.colorState)
-            .oklch,
+          color: matchedColor,
         }}
       >
         CVGen
