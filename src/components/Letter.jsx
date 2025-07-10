549c0c69c2896c7d573638d56431f9b86844d8c6
Hilman Fikry <hilman12fikry@gmail.com>
2025-07-10 10:01:59 +0700

    WIP: create Letter component

--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -1,15 +1,41 @@
+import email from "../assets/email-outline.svg";
+import phone from "../assets/phone-outline.svg";
+import location from "../assets/map-marker-outline.svg";
+import web from "../assets/web.svg";
+
 export default function Letter({ formResult }) {
   return (
     <article>
       <header>
         <div>
-          <h1>{formResult["First name"]} {formResult["Last name"]}</h1>
-          <h2></h2>
-          <p></p>
+          <h1>
+            {formResult["First name"]} {formResult["Last name"]}
+          </h1>
+          <h2>{formResult["Professional title"]}</h2>
+          <p>{formResult["Summary"]}</p>
         </div>
-        <div></div>
+        <div>
+          <div>
+            {formResult["Email"] && <img src={email} alt="Icon" />}
+            <p>{formResult["Email"]}</p>
+          </div>
+          <div>
+            {formResult["Phone number"] && <img src={phone} alt="Icon" />}
+            <p>{formResult["Phone number"]}</p>
+          </div>
+          <div>
+            {formResult["Location"] && <img src={location} alt="Icon" />}
+            <p>{formResult["Location"]}</p>
+          </div>
+          <div>
+            {formResult["Website"] && <img src={web} alt="Icon" />}
+            <p>{formResult["Website"]}</p>
+          </div>
+        </div>
       </header>
-      <section></section>
+      <section>
+        <h3>Education</h3>       
+      </section>
       <section></section>
     </article>
   );
